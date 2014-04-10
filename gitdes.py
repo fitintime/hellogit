4e408438fa66b1b8a002dc19095fb2dd99a3b0b0
lm.pan <lm.pan@tvplay.cn>
2014-04-10 15:28:10 +0800

    modify again

--- gitdes.py
+++ gitdes.py
@@ -1,5 +1,7 @@
 import os, sys
 
 
-print("Git is a version control system.")
-print("Git is free software.")
+if __name__ == "__main__":
+
+	print("Git is a version control system.")
+	print("Git is free software.")
